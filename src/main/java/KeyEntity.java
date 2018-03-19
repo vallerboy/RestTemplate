b27cc63ff8c12efd7e7d98c7db0b2a467e5fc905
Oskar Polak <biuro@fivarto.pl>
2018-03-19 16:55:39 +0100

    ggg

---
+++ src/main/java/KeyEntity.java
@@ -0,0 +1,15 @@
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+
+
+@Data
+@NoArgsConstructor
+public class KeyEntity {
+    private int id;
+    private String key;
+    private String name;
+
+}
---
+++ src/main/java/UserEntity.java
@@ -0,0 +1,21 @@
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+public class UserEntity {
+    private int id;
+    private String username;
+    private String password;
+    private String firstname;
+    private String surname;
+    private boolean isLogin;
+    private LocalDateTime creationDate;
+    private KeyEntity key;
+
+
+}
